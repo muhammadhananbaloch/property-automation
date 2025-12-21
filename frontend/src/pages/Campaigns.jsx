2b99ef6bfb48daf47b541b0d6da9228888541774
Muhammad Hanan Baloch <muhammadhananbaloch@outlook.com>
2025-12-21 15:07:38 +0500

    Add Campaigns component for SMS campaigns

---
+++ frontend/src/pages/Campaigns.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Campaigns = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-slate-900">SMS Campaigns</h1>
+      <p className="text-slate-500 mt-2">Here you will send text blasts to leads.</p>
+    </div>
+  );
+};
+
+export default Campaigns;
